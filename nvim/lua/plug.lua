3c35962d5cc0af10e70cb7db18a5cf1dc8bf2d62
zhangwm404 <442798+zhangwm404@users.noreply.github.com>
2022-05-11 09:41:08 +0800

    nvim: update luasnip and cmp config

--- nvim/lua/plug.lua
+++ nvim/lua/plug.lua
@@ -566,10 +566,59 @@ local packer_startup = function(use)
     -- See https://github.com/neovim/nvim-lspconfig/wiki/Snippets
     use {
         'L3MON4D3/LuaSnip',
+        requires= { 'hrsh7th/nvim-cmp' },
         config = function()
-            require('luasnip').config.setup({
-                store_selection_keys = "<C-i>"
-            })
+            local function prequire(...)
+                local status, lib = pcall(require, ...)
+                if (status) then return lib end
+                return nil
+            end
+
+            local luasnip = prequire('luasnip')
+            local cmp = prequire("cmp")
+
+            local t = function(str)
+                return vim.api.nvim_replace_termcodes(str, true, true, true)
+            end
+
+            local check_back_space = function()
+                local col = vim.fn.col('.') - 1
+                if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+                    return true
+                else
+                    return false
+                end
+            end
+
+            _G.tab_complete = function()
+                if cmp and cmp.visible() then
+                    cmp.select_next_item()
+                elseif luasnip and luasnip.expand_or_jumpable() then
+                    return t("<Plug>luasnip-expand-or-jump")
+                elseif check_back_space() then
+                    return t "<Tab>"
+                else
+                    cmp.complete()
+                end
+                return ""
+            end
+            _G.s_tab_complete = function()
+                if cmp and cmp.visible() then
+                    cmp.select_prev_item()
+                elseif luasnip and luasnip.jumpable(-1) then
+                    return t("<Plug>luasnip-jump-prev")
+                else
+                    return t "<S-Tab>"
+                end
+                return ""
+            end
+
+            vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
+            vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
+            vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+            vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+            vim.api.nvim_set_keymap("i", "<C-E>", "<Plug>luasnip-next-choice", {})
+            vim.api.nvim_set_keymap("s", "<C-E>", "<Plug>luasnip-next-choice", {})
         end
     }
 
@@ -707,7 +756,7 @@ local packer_startup = function(use)
         'mfussenegger/nvim-dap-python', after = { 'nvim-dap' },
         config = function()
             local dappy = require('dap-python')
-            dappy.setup('~/.virtualenvs/debugpy/bin/python')
+            -- dappy.setup('python-path')
             dappy.test_runner = 'pytest'
         end
     }
